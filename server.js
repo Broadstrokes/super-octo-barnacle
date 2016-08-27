49ae5b75fd5df74072e83fd1f1a93df80d18fc86
Dan Sajjad <dan.sajjad@gmail.com>
2016-08-27 15:45:58 -0400

    Set pug as view engine, add middleware, create views and public folder

--- server.js
+++ server.js
@@ -6,8 +6,40 @@ const
 const app  = express();
 let port = process.env.PORT || 3000;
 
-app.get('/',  (req, res) => res.send('Hello world!'));
+(str, path) => {
+  stylus(str)
+  .set('filename', path)
+  .use(nib())
+} 
+/*
+app.set directives tell Express where we keep our views &
+to use pug 
+*/
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+/*
+app.use calls pass middleware function for Express to use
+Middleware are simply functions that have the signature fn(req, res, next)
+*/
+/*
+The first piece of middleware we apply is the Express logger in 'dev' mode. This will simply log incoming requests to the console
+*/
+app.use(express.logger('dev'))
+/*
+Next, the Stylus middleware is applied, which will compile our .styl files to CSS.
+In order to use nib, we pass in a custom compile function to the Stylus middleware. 
+*/
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}))
+/*
+After that it's the Express static middleware, which is used for serving static files (we tell it that our static files will live in a folder called 'public', so lets create that in the project root now)
+*/
+app.use(express.static(__dirname + '/public'));
 
+
+app.get('/',  (req, res) => res.send('Hello world!'));
 
 app.listen(port, () => 
   console.log(`Listening on port ${port}, in ${process.env.NODE_ENV} mode!`));
